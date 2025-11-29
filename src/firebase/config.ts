231b151b0dde909891c375bcec7610edcb2a97e5
Ortem Titov <titovortem@gmail.com>
2025-11-29 21:10:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6506257696-776e2",
+  "appId": "1:974138790726:web:49027db75a7a4ea26ccb47",
+  "apiKey": "AIzaSyCrxxTrw-vQ6zs7U3jYVtOpqUtN-Yl6HT4",
+  "authDomain": "studio-6506257696-776e2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "974138790726"
+};
