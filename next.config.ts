77596ee1587c8105d5c041b3a4ca5058f38cca2d
Ortem Titov <titovortem@gmail.com>
2025-11-30 12:09:38 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'img.dmclk.ru',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sun9-56.userapi.com',
         port: '',
         pathname: '/**',
       },
