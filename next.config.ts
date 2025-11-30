06619deb5b18292426d6371f63750deea29a2e13
Ortem Titov <titovortem@gmail.com>
2025-11-30 12:26:08 +0000

    https://sun9-20.userapi.com/s/v1/ig2/uIdtH8WL93WBwVCjfTiSSkbYOzSbkBJsjhG

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'sun9-20.userapi.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
